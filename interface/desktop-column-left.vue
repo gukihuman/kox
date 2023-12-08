8bc60c68678db7b5bb0f44a5e61a2e1af829e28a
ivan-f-sokolov <sokolovivanf@gmail.com>
2023-12-09 03:11:54 +0500

    desktop styling

--- interface/desktop-column-left.vue
+++ interface/desktop-column-left.vue
@@ -1,8 +1,45 @@
 <template lang="pug">
 div(class="w-[525px] h-full mx-auto flex flex-col gap-[28px]")
+  div(class="w-full h-[26px] bg-[#D5AD90] flex justify-center mt-[8px] mb-[-10px]")
+    desktop-text(text="О продукте" bold="true")
   desktop-text(indent="true" text="Продукт заполняет пустующую на рынке нишу натурального, классического копчёного сыра. Отлично подходит к чаю, вину, пиву. Идеален в дальней дороге и на изысканном столе. Приятные ноты можжевелового аромата прекрасно сочетаются с классическим вкусом лучших алтайских сыров.")
 
-  img(src="@/assets/photo-2.webp" alt="Фото сыра" class="w-full object-cover rounded-[12px] select-none shadow-lg")
+  img(src="@/assets/photo-4.webp" alt="Фото сыра" class="w-full object-cover rounded-[12px] select-none shadow-lg")
 
-  img(src="@/assets/photo-1.webp" alt="Фото прилавка с сыром" class="w-full object-cover rounded-[12px] select-none mt-[10px] shadow-lg")
+  div(class="h-[70px]")
+    div(class="w-full h-[26px] bg-[#D5AD90] flex items-center mt-[8px] mb-[-10px] flex flex-col")
+      desktop-text(text="Партнёры" class="ml-[-24px]" bold="true" class="mb-[6px]")
+      desktop-text(text="(здесь вы можете приобрести сыр)" class="ml-[-24px]")
+
+  desktop-text(text="«Cleverfich»" class="w-[400px]" bold="true" class="mt-[-10px]")
+  desktop-text(text="магазин рыбы и морепродуктов" class="w-[400px]" class="mt-[-54px] ml-[145px]")
+  desktop-text(text="г. Пермь, Комсомольский пр-т, д. 49" class="mt-[-18px]")
+  desktop-text(text="г. Пермь, Курья, Лесопарковый переулок 2а/3" class="mt-[-18px]")
+  
+  desktop-text(text="«Скажите сыр»" class="w-[400px]" bold="true" class="mt-[-10px]")
+  desktop-text(text="г. Пермь, ул. Сибирская, д. 46. " class="mt-[-18px]")
+
+  desktop-text(text="«Бабушкина грядка»" class="w-[400px]" bold="true" class="mt-[-10px]")
+  desktop-text(text="г. Пермь, ул. Газеты звезда, д.30" class="mt-[-18px]")
+
+  desktop-text(text="«Карин»" class="w-[400px]" bold="true" class="mt-[-10px]")
+  desktop-text(text="ресторан" class="w-[400px]" class="mt-[-54px] ml-[105px]")
+  desktop-text(text="г. Пермь, б-р Гагарина д. 65, стр. а" class="mt-[-18px]")
+
+  desktop-text(text="«VINORIO»" class="w-[400px]" bold="true" class="mt-[-10px]")
+  desktop-text(text="винный бар – магазин" class="w-[400px]" class="mt-[-54px] ml-[130px]")
+  desktop-text(text="г. Пермь, ул. Советская д. 65, стр. а" class="mt-[-18px]")
+
+  desktop-text(text="«Партизан»" class="w-[400px]" bold="true" class="mt-[-10px]")
+  desktop-text(text="ресторан" class="w-[400px]" class="mt-[-54px] ml-[142px]")
+  desktop-text(text="Комсомольский пр-т, д. 44, стр. а" class="mt-[-18px] mb-[2px]")
+
+  img(src="@/assets/photo-1.webp" alt="Фото сыра" class="w-full object-cover rounded-[12px] select-none mt-[10px] shadow-lg")
+
+  div(class="w-full h-[26px] bg-[#D5AD90] flex justify-center mt-[8px] mb-[0px]")
+    desktop-text(text="Контакты" bold="true")
+  desktop-text(text="Адрес производства:" class="mt-[-10px]")
+  desktop-text(text="г. Кунгур, ул. Голдобина 2а" class="mt-[-22px]")
+  desktop-text(text="Телефон: 8 951 941 30 30 " class="mt-[-10px]")
+  desktop-text(text="Почта: koh59@yandex.ru " class="mt-[-10px]")
 </template>
--- interface/roof.vue
+++ interface/roof.vue
@@ -1,9 +1,8 @@
 <template lang="pug">
-div(class="relative w-full h-[105px] md:h-[275px]")
+div(class="relative w-full h-[105px] md:h-[210px]")
   div(class="absolute w-full h-[75px] md:h-[145px] mt-[15px] md:mt-[30px] bg-[#dcb79c]/80 flex items-center")
-    div(class="ml-[75px]")
-    h1(class="mt-[0px] md:mt-[15px] ml-[30px] md:ml-[260px] text-[32px] md:text-[52px] font-russo text-chocolate-cosmos") СЫР
-    h2(class="mt-[8px] md:mt-[-7px] ml-[12px] md:ml-[260px] text-[16px] md:text-[31px] font-russo text-chocolate-cosmos text-center leading-[18px]") КОПЧЁНЫЙ НА <br> МОЖЖЕВЕЛЬНИКЕ
-  div(class="absolute w-[70px] md:w-[140px] h-[80px] md:h-[160px] ml-[16px] md:ml-[912px] bg-gradient-to-b from-[#713d1a] to-[#dd992f] rounded-bl-[10px] rounded-br-[10px] shadow-lg")
+    h1(class="mt-[0px] md:mt-[15px] ml-[30px] md:ml-[255px] text-[28px] md:text-[48px] font-russo text-chocolate-cosmos") СЫР
+    h2(class="mt-[0px] md:mt-[20px] ml-[100px] md:ml-[260px] text-[16px] md:text-[31px] font-russo text-chocolate-cosmos leading-[18px] md:leading-[32px]") КОПЧЁНЫЙ НА <br> МОЖЖЕВЕЛЬНИКЕ
+  div(class="absolute w-[70px] md:w-[140px] h-[80px] md:h-[160px] ml-[110px] md:ml-[435px] bg-gradient-to-b from-[#713d1a] to-[#dd992f] rounded-bl-[10px] rounded-br-[10px] shadow-lg")
     img(class="mx-auto w-[58px] md:w-[120px] mt-[15px] md:mt-[30px] rounded-[3px] select-none" src="@/assets/logo.webp")
 </template>
